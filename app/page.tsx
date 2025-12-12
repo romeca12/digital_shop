30e588acce9d57738e56ab05c521295f606ae9d3
mohammad bagher <warken1290@gmail.com>
2025-12-12 10:19:18 +0330

    remove variable

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
 export default function Home() {
 
-
-  const name = "12";
-
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black"></div>
   );
