1eb3b85fa5ae6ed1b73e97e9e065ba9011500b2c
mohammad bagher <warken1290@gmail.com>
2025-12-12 10:15:53 +0330

    add husky

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
-  const name = '12';
+
+
+  const name = "12";
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black"></div>
